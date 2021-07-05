f1dfa7d1b1a594a249ed476b8d74598a9f48b167
Erica <ericaspitz522@gmail.com>
2021-07-05 17:52:15 -0600

    Build out Turn class and pass function/instanceOf tests

--- test/Turn-test.js
+++ test/Turn-test.js
@@ -10,4 +10,12 @@ describe('Turn', () => {
     card = new Card(1, 'What allows you to define a set of related information using key-value pairs?', ['object', 'array', 'function'], 'object');
     turn = new Turn('array', card);
   });
-})
+
+  it('should be a function', () => {
+    expect(Turn).to.be.a('function');
+  });
+
+  it('should instantiate a new turn', () => {
+    expect(turn).to.be.an.instanceof(Turn);
+  });
+});
