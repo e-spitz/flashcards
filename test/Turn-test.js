953554b5b6882e7a8747f2677c86d77866b3a2ca
Erica <ericaspitz522@gmail.com>
2021-07-05 17:48:34 -0600

    Create turn test file, describe block, and beforeEach hook

---
+++ test/Turn-test.js
@@ -0,0 +1,13 @@
+const chai = require('chai');
+const expect = chai.expect;
+const Card = require('../src/Card');
+const Turn = require('../src/Turn');
+
+describe('Turn', () => {
+  let card, turn;
+
+  beforeEach(() => {
+    card = new Card(1, 'What allows you to define a set of related information using key-value pairs?', ['object', 'array', 'function'], 'object');
+    turn = new Turn('array', card);
+  });
+})
