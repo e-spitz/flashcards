11bd9cbe5545347943420aec534edfccc19963b6
Erica <ericaspitz522@gmail.com>
2021-07-06 12:20:12 -0600

    Create round/round test files and setup

---
+++ test/Round-test.js
@@ -0,0 +1,19 @@
+const chai = require('chai');
+const expect = chai.expect;
+const Card = require('../src/Card.js');
+const Deck = require('../src/Deck.js');
+const Round = require('../src/Round.js');
+
+describe('Round', () => {
+  let card1, card2, card3, deck, round;
+
+  beforeEach(() => {
+    card1 = new Card(1, 'What allows you to define a set of related information using key-value pairs?', ['object', 'array', 'function'], 'object');
+    card2 = new Card(2, 'What is a comma-separated list of related values?', ['array', 'object', 'function'], 'array');
+    card3 = new Card(3, 'What type of prototype method directly modifies the existing array?', ['mutator method', 'accessor method', 'iteration method'], 'mutator method');
+    deck = new Deck([card1, card2, card3]);
+    round = new Round(deck);
+  });
+
+
+});
