5a087a94af757b3175c30b47ae53f42e5393a1c8
Erica <ericaspitz522@gmail.com>
2021-07-05 19:20:50 -0600

    Create cards property and test

--- test/Deck-test.js
+++ test/Deck-test.js
@@ -10,7 +10,7 @@ describe('Deck', () => {
     card1 = new Card(1, 'What allows you to define a set of related information using key-value pairs?', ['object', 'array', 'function'], 'object');
     card2 = new Card(2, 'What is a comma-separated list of related values?', ['array', 'object', 'function'], 'array');
     card3 = new Card(3, 'What type of prototype method directly modifies the existing array?', ['mutator method', 'accessor method', 'iteration method'], 'mutator method');
-    deck = new Deck(card1, card2, card3);
+    deck = new Deck([card1, card2, card3]);
   });
 
   it('should be a function', () => {
@@ -20,4 +20,9 @@ describe('Deck', () => {
   it('should be an instance of Deck', () => {
     expect(deck).to.be.an.instanceof(Deck);
   });
+
+  it('should store cards', () => {
+    expect(deck.cards).to.deep.equal([card1, card2, card3])
+  })
+
 });
