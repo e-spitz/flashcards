a4f62c6e97f09ed1695a4e6c2a0331a381ff3c39
Erica <ericaspitz522@gmail.com>
2021-07-07 15:28:13 -0600

    Add another test to check game start method and instantiations

--- test/Game-test.js
+++ test/Game-test.js
@@ -1,7 +1,11 @@
 const chai = require('chai');
 const expect = chai.expect;
+const data = require('../src/data');
+const prototypeQuestions = data.prototypeData;
 const Round = require('../src/Round');
 const Game = require('../src/Game')
+const Card = require('../src/Card')
+const Deck = require('../src/Deck')
 
 describe('Game', () => {
   let game;
@@ -22,12 +26,11 @@ describe('Game', () => {
     expect(game.currentRound).to.equal(0);
   });
 
- //  it('should create a new Round using the Deck', () => {
- //   game.start();
- //   const currentDeck = game.currentRound.deck;
- //
- //   expect(currentDeck.cards[0]).to.be.an.instanceof(Card);
- //   expect(currentDeck).to.be.an.instanceof(Deck);
- //   expect(currentDeck.cards.length).to.equal(prototypeQuestions.length);
- // });
+  it('should create a new Round using the Deck', () => {
+   game.start();
+   const currentDeck = game.currentRound.deck;
+   expect(currentDeck.cards[0]).to.be.an.instanceof(Card);
+   expect(currentDeck).to.be.an.instanceof(Deck);
+   expect(currentDeck.cards.length).to.equal(prototypeQuestions.length);
+ });
 });
